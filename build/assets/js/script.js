82c2595e54765406c9d8bff07d880648c54656e4
Apprentice 2 <apprentice2@apprentice2.local>
2014-04-01 17:28:38 -0400

    updates to html5 validation messages

--- build/assets/js/script.js
+++ build/assets/js/script.js
@@ -1,1 +1,42 @@
+//When the browser is ready...
+
+$(function()) {
+
+	//Setup the form validation on the #form element
+
+	$("#form").validate({
+
+		rules: {
+
+			pword: {
+
+				required: true;
+				minlength: 5
+
+			},
+			
+			//Specity the validation error messages
+			messages: {
+
+				pword: {
+					required: "Please provide a password",
+					minlength: "Your password must be at least 5 characters long" 
+
+				}, //password
+
+			},
+
+			submitHandler: function(form) {
+				form.submit();
+			}
+
+		});
+	
+
+});
+
+
+
+}
+
 
